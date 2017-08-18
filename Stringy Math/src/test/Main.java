79260fcd8d42d803433e3da02be4ff6cf7572c4e
Theodore Clapp <teddy.clapp@gmail.com>
2017-08-17 19:20:46 -0700

    Shallow clone bug fix

--- Stringy Math/src/test/Main.java
+++ Stringy Math/src/test/Main.java
@@ -1,10 +1,26 @@
 package test;
 
 import arithmetic.numbers.DynamicNumber;
+import logical.utils.LogicalUtils;
 
 public class Main {
-
 	public static void main(String... strings) {
-		new DynamicNumber("5").multiply("9").println();
+		DynamicNumber number = new DynamicNumber("53656.15452").subtract("-9.45587000000512").add("358.2").subtract("-8.1");
+
+		DynamicNumber max = new DynamicNumber("10");
+		DynamicNumber index = new DynamicNumber("0");
+
+		/**
+		 * while(index < max) {
+		 * 	number -= .001500000000040901;
+		 *  index++;
+		 * }
+		 */
+		while (LogicalUtils.getLogicalStatus(index, max) == "false") {
+			number.subtract(".001500000000040901");
+			index.add("1");
+			index.println();
+		}
+		number.println();
 	}
 }
