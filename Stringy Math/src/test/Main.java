906bd0da7f5accaa426e690ddc8f6a19dfd6f868
Theodore Clapp <teddy.clapp@gmail.com>
2017-10-13 21:57:40 -0700

    division update

--- Stringy Math/src/test/Main.java
+++ Stringy Math/src/test/Main.java
@@ -4,7 +4,7 @@ import arithmetic.numbers.DynamicNumber;
 
 public class Main {
 	public static void main(String... strings) {
-		DynamicNumber number = new DynamicNumber("11.1");
-		number.divide("4").println("Final Answer");
+		DynamicNumber number = new DynamicNumber("100");
+		number.divide("2").println("Final Answer");
 	}
 }
