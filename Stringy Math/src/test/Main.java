7d92c582011746ee573e11d395e10e511498140b
Theodore Clapp <teddy.clapp@gmail.com>
2017-10-16 14:56:17 -0700

    Mul is working perfectly
    The decimal placement for div is the only thing that lacks

--- Stringy Math/src/test/Main.java
+++ Stringy Math/src/test/Main.java
@@ -4,7 +4,7 @@ import arithmetic.numbers.DynamicNumber;
 
 public class Main {
 	public static void main(String... strings) {
-		DynamicNumber number = new DynamicNumber("-1111").divide("1000");
+		DynamicNumber number = new DynamicNumber("5.2").multiply("9.2");
 		number.formatNumber();
 		number.println();
 	}
